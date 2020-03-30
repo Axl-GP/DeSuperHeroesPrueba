f07458cb610c14097a784fe83a2574ad3829413e
Wilmer <wilmermanuel.guerrero25@gmail.com>
2020-03-30 17:01:48 -0400

    Modelacion de la entidad stock terminada.

--- DeSuperHeroesPrueba/Models/stock.cs
+++ DeSuperHeroesPrueba/Models/stock.cs
@@ -1,11 +1,31 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DeSuperHeroesPrueba.Models
 {
+    [Table("stock")]
     public class stock
     {
+        [Key]
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string RNC { get; set; }
+        public string telefono { get; set; }
+        public string email { get; set; }
+
+        public class mapear
+        {
+            public mapear(EntityTypeBuilder<stock> mapeo)
+            {
+                mapeo.HasKey(x => x.id);
+            }
+        }
+
+
     }
 }
