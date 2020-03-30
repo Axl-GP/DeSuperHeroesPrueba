3d15d715508d1d954c0beb32289165eb4ffa4c39
Wilmer <wilmermanuel.guerrero25@gmail.com>
2020-03-30 17:50:58 -0400

    modelacion de la entidad producto_proveedor, terminada

---
+++ DeSuperHeroesPrueba/Models/producto.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeSuperHeroesPrueba.Models
+{
+    [Table("producto")]
+    public class producto
+    {
+        [Key]
+        public int id { get; set; }
+        public int idStock { get; set; }
+        [Required, MinLength(3), MaxLength(40)]
+        public string nombre { get; set; }
+        [Required]
+        public double precio { get; set; }
+        public virtual stock Stock { get; set; }
+
+
+        public class mapear
+        {
+            public mapear(EntityTypeBuilder<producto> mapeo)
+            {
+                mapeo.HasKey(x => x.id);
+            }
+        }
+    }
+}
---
+++ DeSuperHeroesPrueba/Models/producto_proveedor.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeSuperHeroesPrueba.Models
+{
+    [Table("producto_proveedor")]
+    public class producto_proveedor
+    {
+        [Key]
+        public int idproducto_proveedor { get; set; }
+
+     
+
+        public int idproducto { get; set; }
+
+      
+
+        public int idproveedor { get; set; }
+
+        [Required]
+        public int cantidad { get; set;
+        }
+        [Required]
+        public DateTime fechaImporte { get; set; }
+
+        public virtual producto producto{get;set;}
+
+        public virtual proveedor proveedor { get; set; }
+
+        class mapear
+        {
+            public mapear(EntityTypeBuilder<producto_proveedor> mapeo)
+            {
+                mapeo.HasKey(x => new { x.idproducto, x.idproducto_proveedor, x.idproveedor });
+            }
+        }
+    }
+}
