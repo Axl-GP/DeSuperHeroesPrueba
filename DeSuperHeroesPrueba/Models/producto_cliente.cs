c4e0748cb3c866042730085f330e73a4fe177945
Wilmer <wilmermanuel.guerrero25@gmail.com>
2020-03-30 18:06:18 -0400

    modelado de la entidad producto_cliente terminada

--- DeSuperHeroesPrueba/Models/producto_cliente.cs
+++ DeSuperHeroesPrueba/Models/producto_cliente.cs
@@ -1,11 +1,36 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DeSuperHeroesPrueba.Models
 {
+    [Table("producto_cliente")]
     public class producto_cliente
     {
+        [Key]
+
+        public int idproducto_cliente { get; set; }
+        public int idcliente { get; set; }
+        public int idproducto { get; set; }
+        [Required]
+        public int cantidad { get; set; }
+    
+        public DateTime fechaFactura { get; set; }
+
+        public virtual producto producto { get; set; }
+        public virtual Cliente cliente { get; set; }
+
+        public class mapear
+        {
+            public mapear(EntityTypeBuilder<producto_cliente> mapeo)
+            {
+                mapeo.HasKey(x => new { x.idproducto, x.idcliente, x.idproducto_cliente });
+
+            }
+        }
     }
 }
