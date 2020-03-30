c7cb82ae7f74abb8b2fbff25756ea3597faa36a3
Wilmer <wilmermanuel.guerrero25@gmail.com>
2020-03-30 17:18:01 -0400

    modelado de la entidad factura, terminada.

---
+++ DeSuperHeroesPrueba/Models/Factura.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeSuperHeroesPrueba.Models
+{
+    [Table("factura")]
+    public class Factura
+    {
+        [Key]
+        public int id { get; set; }
+        
+        public int idCliente { get; set; }
+
+        [Required, Range(1,50)]
+        public int cantidad { get; set; }
+
+        [Required]
+        public double total { get; set; }
+        [Required]
+        public DateTime fecha { get; set; }
+        public virtual Cliente Cliente { get; set; }
+
+        class mapear
+        {
+            public mapear(EntityTypeBuilder<Factura> mapeo)
+            {
+                mapeo.HasKey(x => x.id);
+            }
+        }
+    }
+}
