789a2dc0a50dcee2df21a81b5f7d3843fdc06fa3
Wilmer <wilmermanuel.guerrero25@gmail.com>
2020-03-30 17:13:18 -0400

    Modelado de la entidad proveedor terminada

--- DeSuperHeroesPrueba/Models/proveedor.cs
+++ DeSuperHeroesPrueba/Models/proveedor.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+﻿
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -8,29 +9,30 @@ using System.Threading.Tasks;
 
 namespace DeSuperHeroesPrueba.Models
 {
-    [Table("stock")]
-    public class stock
+    [Table("proveedor")]
+    public partial class proveedor
     {
         [Key]
         public int id { get; set; }
-
         [Required, MinLength(3), MaxLength(40)]
         public string nombre { get; set; }
         [Required]
+
         public string RNC { get; set; }
         [Required]
         public string telefono { get; set; }
-        [Required, EmailAddress]
+
+        [Required,EmailAddress]
         public string email { get; set; }
 
-         class mapear
+        class mapear
         {
-            public mapear(EntityTypeBuilder<stock> mapeo)
+            public mapear(EntityTypeBuilder<proveedor> mapeo)
             {
                 mapeo.HasKey(x => x.id);
             }
         }
-
-
     }
+
+    
 }
