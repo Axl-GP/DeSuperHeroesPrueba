8cd1bb039d2b88fe91c82c889907d417fa5ae39a
Wilmer <wilmermanuel.guerrero25@gmail.com>
2020-03-30 17:00:29 -0400

    Modelacion de la entidad cliente completada

--- DeSuperHeroesPrueba/Models/Cliente.cs
+++ DeSuperHeroesPrueba/Models/Cliente.cs
@@ -1,1 +1,36 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeSuperHeroesPrueba.Models
+{
+    [Table("cliente")]
+    public partial class Cliente
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required, MinLength(3), MaxLength(50)]
+        public string nombre { get; set; }
+        [Required]
+        public string RNC { get; set; }
+        [Required,Phone]
+        public string telefono { get; set; }
+        [Required, EmailAddress]
+        public string email { get; set; }
+        [Required]
+        public categoria categoria { get; set; }
+    }
+    public enum categoria { Premium, Regular }
+
+    class mapear
+    {
+        public mapear(EntityTypeBuilder<Cliente> mapeo)
+        {
+            mapeo.HasKey(x => x.ID);
+        }
+    }
+}
